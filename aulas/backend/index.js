415106449d86ef2b02abc8c2273415540c13929b
Jorge Hecherat <hecherat@gmail.com>
2020-03-24 14:49:11 -0300

    [backend] - information added

--- aulas/backend/index.js
+++ aulas/backend/index.js
@@ -2,7 +2,20 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (request, response) => {
+/**
+ * Rota / Recurso
+ */
+
+/**
+ * Métodos HTTP:
+ *
+ * GET: Buscar uma informação do back-end
+ * POST: Criar uma informação no back-end
+ * PUT: Alterar uma informação no back-end
+ * DELETE: Deletar uma informação no back-end
+ */
+
+app.get("/users", (request, response) => {
   return response.json({
     evento: "Semana Omnistack 11.0",
     aluno: "Jorge Hecherat"
