12e39f4ad3e0932cdd29cd33dae6b6b8c08e04d2
Jorge Hecherat <hecherat@gmail.com>
2020-03-25 11:37:33 -0300

    [backend] - Parameter types

--- aulas/backend/index.js
+++ aulas/backend/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 
 const app = express();
 
+// Converte o json do corpo da requisição em um objeto do javascript
+app.use(express.json());
+
 /**
  * Rota / Recurso
  */
@@ -15,7 +18,30 @@ const app = express();
  * DELETE: Deletar uma informação no back-end
  */
 
-app.get("/users", (request, response) => {
+/**
+ * Tipos de parâmetros:
+ *
+ * Query: Parâmetros nomeados enviados na rota após "?" (Filtros, páginação)
+ *  acesso através de "request.query"
+ *  ex: /users?nome=Jorge&ideade=30
+ * Route Params: Parâmetros utilizados para identificar recursos
+ *  acesso através de "request.params"
+ *  ex: /users/:id => /users/1
+ * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
+ *  acesso através de "request.body"
+ *  ex: método POST
+ * body = {
+    "nome": "Jorge Hecherat",
+	  "idade": 30
+   }
+   header = Content-Type: application/json
+ */
+
+app.post("/users", (request, response) => {
+  const params = request.body;
+
+  console.log(params);
+
   return response.json({
     evento: "Semana Omnistack 11.0",
     aluno: "Jorge Hecherat"
