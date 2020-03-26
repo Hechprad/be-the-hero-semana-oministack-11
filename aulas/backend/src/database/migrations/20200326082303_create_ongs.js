5eecfbd3f9f73265808c05ee4f619780f7bcc36c
Jorge Hecherat <hecherat@gmail.com>
2020-03-26 09:01:24 -0300

    [backend] - First table created with npx knex migrate:latest

--- aulas/backend/src/database/migrations/20200326082303_create_ongs.js
+++ aulas/backend/src/database/migrations/20200326082303_create_ongs.js
@@ -1,8 +1,14 @@
-
-exports.up = function(knex) {
-  
+exports.up = knex => {
+  return knex.schema.createTable("ongs", table => {
+    table.string("id").primary();
+    table.string("name").notNullable();
+    table.string("email").notNullable();
+    table.string("whatsapp").notNullable();
+    table.string("city").notNullable();
+    table.string("uf", 2).notNullable();
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = knex => {
+  return knex.schema.dropTable("ongs");
 };
