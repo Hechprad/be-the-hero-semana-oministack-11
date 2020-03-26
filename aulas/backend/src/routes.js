ea87f4eb86f922d81256039a82dcf5fd994c1009
Jorge Hecherat <hecherat@gmail.com>
2020-03-26 14:50:34 -0300

    [backend] - New route and session controller created to manage the application login

--- aulas/backend/src/routes.js
+++ aulas/backend/src/routes.js
@@ -1,10 +1,14 @@
 const express = require("express");
 
+const SessionController = require("./controllers/SessionController");
+
 const OngController = require("./controllers/OngController");
 const IncidentController = require("./controllers/IncidentController");
 const ProfileController = require("./controllers/ProfileController");
 
 const routes = express.Router();
+
+routes.post("/sessions", SessionController.create);
 
 routes.get("/ongs", OngController.index);
 routes.post("/ongs", OngController.create);
