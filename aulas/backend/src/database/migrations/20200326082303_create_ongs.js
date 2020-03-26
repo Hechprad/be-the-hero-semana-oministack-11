c620f3e2abf3785973f1872ff6fff36e896b8f87
Jorge Hecherat <hecherat@gmail.com>
2020-03-26 10:09:48 -0300

    [backend] - Second table created with npx knex migrate:latest

--- aulas/backend/src/database/migrations/20200326082303_create_ongs.js
+++ aulas/backend/src/database/migrations/20200326082303_create_ongs.js
@@ -1,6 +1,12 @@
+/**
+ * npx knex migrate:make create_ongs > execute to create this file
+ * set the table schema
+ * npx knex migrate:latest
+ */
 exports.up = knex => {
   return knex.schema.createTable("ongs", table => {
-    table.string("id").primary();
+    table.string("id").primary(); //Primary Key
+
     table.string("name").notNullable();
     table.string("email").notNullable();
     table.string("whatsapp").notNullable();
