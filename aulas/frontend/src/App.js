d7e792f956751e7211d78baa8cf2207b202d57e6
Jorge Hecherat <hecherat@gmail.com>
2020-03-26 23:42:05 -0300

    [frontend] Header component

--- aulas/frontend/src/App.js
+++ aulas/frontend/src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
+import Header from "./components/Header";
 
 function App() {
-  return <h1>Hello World</h1>;
+  return <Header>Be The Hero</Header>;
 }
 
 export default App;
