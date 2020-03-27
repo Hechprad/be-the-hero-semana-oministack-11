9cf592736df0603c252f15afae40e887f969fd41
Jorge Hecherat <hecherat@gmail.com>
2020-03-27 08:37:24 -0300

    [frontend] - Logon page basic structure

--- aulas/frontend/src/pages/Logon/index.js
+++ aulas/frontend/src/pages/Logon/index.js
@@ -1,9 +1,24 @@
 import React from "react";
 
-import { Title } from "./styles";
+import logoImg from "../../assets/logo.svg";
+import heroesImg from "../../assets/heroes.png";
 
-const Logon = () => {
-  return <Title>Hello World</Title>;
-};
+const Logon = () => (
+  <div className="logon-container">
+    <section className="form">
+      <img src={logoImg} alt="Be The Hero" />
+
+      <form>
+        <h1>Faça seu logon</h1>
+
+        <input placeholder="Sua ID" />
+        <button type="submit">Entrar</button>
+
+        <a href="/register">Não tenho cadastro</a>
+      </form>
+    </section>
+    <img src={heroesImg} alt="Heroes" />
+  </div>
+);
 
 export default Logon;
