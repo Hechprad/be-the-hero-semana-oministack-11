c69fd4e96be7ef020246707cc731a838b69f1cbc
Jorge Hecherat <hecherat@gmail.com>
2020-03-27 10:03:24 -0300

    [frontend] - branch fix

---
+++ aulas/frontend/src/styles/theme.js
@@ -0,0 +1,13 @@
+export const themeColors = {
+  background: "#f0f0f5",
+  black: "#000",
+  darkGray: "#333",
+  gray1: "#dcdce6",
+  red: "#e02041",
+  rocketSeatPurple: "#7159c1",
+  white: "#FFFFFF"
+};
+
+export default {
+  colors: themeColors
+};
