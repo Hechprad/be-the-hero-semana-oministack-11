7ac18bf0fcad9bdca09ab99bb3666fc9c40c11c5
Jorge Hecherat <hecherat@gmail.com>
2020-03-30 15:17:31 -0300

    [frontend] - Component Button update

--- aulas/frontend/src/components/Button/index.js
+++ aulas/frontend/src/components/Button/index.js
@@ -2,6 +2,8 @@ import React from "react";
 
 import { Button } from "./styles";
 
-const LogonButton = ({ ...rest }) => <Button {...rest}>Entrar</Button>;
+const LogonButton = ({ children, ...rest }) => (
+  <Button {...rest}>{children ? children : "Entrar"}</Button>
+);
 
 export default LogonButton;
