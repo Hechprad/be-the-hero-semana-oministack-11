0a90b484d4439e1e97b7cc04dcd700e60d3f4628
Jorge Hecherat <hecherat@gmail.com>
2020-03-31 19:48:37 -0300

    [frontend] - IncidentList refactor

--- aulas/frontend/src/components/IncidentList/index.js
+++ aulas/frontend/src/components/IncidentList/index.js
@@ -1,28 +1,65 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from "../../services/api";
 
 import { FiTrash2Icon, Li, ListButton, StrongText, Text, Ul } from "./styles";
 
 export const IncidentList = ({ content, ...rest }) => {
-  return content ? (
-    <Ul {...rest}>
-      {content.map(incidentItem => (
-        <Li key={incidentItem.id}>
-          <StrongText>CASO:</StrongText>
-          <Text>{incidentItem.title}</Text>
+  const [incidentsData, setIncidentsData] = useState([]);
+  const ongId = localStorage.getItem("ongId");
 
-          <StrongText>DESCRIÇÃO:</StrongText>
-          <Text>{incidentItem.description}</Text>
+  useEffect(() => {
+    api
+      .get("profile", {
+        headers: { Authorization: ongId }
+      })
+      .then(response => {
+        setIncidentsData(response.data);
+      });
+  }, [ongId]);
 
-          <StrongText>VALOR:</StrongText>
-          <Text>R$ {incidentItem.value}</Text>
+  const handleDeleteItem = async caseId => {
+    try {
+      await api.delete(`incidents/${caseId}`, {
+        headers: {
+          Authorization: ongId
+        }
+      });
 
-          <ListButton>
-            <FiTrash2Icon />
-          </ListButton>
-        </Li>
-      ))}
-    </Ul>
-  ) : null;
+      setIncidentsData(
+        incidentsData.filter(incident => incident.id !== caseId)
+      );
+    } catch (err) {
+      alert("Erro ao deletar caso, tente novamente.");
+    }
+  };
+
+  return (
+    incidentsData.length > 0 && (
+      <Ul {...rest}>
+        {incidentsData.map(incidentItem => (
+          <Li key={incidentItem.id}>
+            <StrongText>CASO:</StrongText>
+            <Text>{incidentItem.title}</Text>
+
+            <StrongText>DESCRIÇÃO:</StrongText>
+            <Text>{incidentItem.description}</Text>
+
+            <StrongText>VALOR:</StrongText>
+            <Text>
+              {Intl.NumberFormat("pt-BR", {
+                style: "currency",
+                currency: "BRL"
+              }).format(incidentItem.value)}
+            </Text>
+            <ListButton onClick={() => handleDeleteItem(incidentItem.id)}>
+              <FiTrash2Icon />
+            </ListButton>
+          </Li>
+        ))}
+      </Ul>
+    )
+  );
 };
 
 export default IncidentList;
--- aulas/frontend/src/pages/Profile/index.js
+++ aulas/frontend/src/pages/Profile/index.js
@@ -1,6 +1,4 @@
-import React, { useState, useEffect } from "react";
-
-import api from "../../services/api";
+import React from "react";
 
 import logoImg from "../../assets/logo.svg";
 
@@ -18,37 +16,20 @@ import {
 } from "./styles";
 
 export const Profile = () => {
-  const [incidentsData, setIncidentsData] = useState([]);
-  const ongId = localStorage.getItem("ongId");
   const ongName = localStorage.getItem("ongName");
-
-  useEffect(() => {
-    api
-      .get("profile", {
-        headers: { Authorization: ongId }
-      })
-      .then(response => {
-        setIncidentsData(response.data);
-      });
-  }, [ongId]);
-
-  console.log(incidentsData);
 
   return (
     <ProfileContainer>
       <Header>
         <Img src={logoImg} alt="Be The Hero" />
         <HeaderText>{`Bem vinda, ${ongName}`}</HeaderText>
-
         <Link to="/incidents/new">Cadastrar novo caso</Link>
         <Button>
           <FiPowerIcon />
         </Button>
       </Header>
-
       <Title>Casos cadastrados</Title>
-
-      {incidentsData.length > 0 && <IncidentList content={incidentsData} />}
+      <IncidentList />
     </ProfileContainer>
   );
 };
