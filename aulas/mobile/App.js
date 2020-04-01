1be631e1899c77c03c7e3a597da08908b2ef74e2
Jorge Hecherat <hecherat@gmail.com>
2020-04-01 12:06:25 -0300

    [mobile] - Theme and routes import at App.js

--- aulas/mobile/App.js
+++ aulas/mobile/App.js
@@ -1,10 +1,12 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { ThemeProvider } from "styled-components";
+import Routes from "./src/routes";
+import theme from "./src/styles/theme";
 
 export default function App() {
   return (
-    <View>
-      <Text>Hello Omnistack</Text>
-    </View>
+    <ThemeProvider theme={theme}>
+      <Routes />
+    </ThemeProvider>
   );
 }
