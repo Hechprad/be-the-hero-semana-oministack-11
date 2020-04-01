3620d5dbab4080fb51fe85bcfd024cebcfdaaf4d
Jorge Hecherat <hecherat@gmail.com>
2020-04-01 12:05:10 -0300

    [mobile] - pages content fix

--- aulas/mobile/src/pages/Detail/index.js
+++ aulas/mobile/src/pages/Detail/index.js
@@ -3,5 +3,5 @@ import React from "react";
 import { View } from "./styles";
 
 export default function Details() {
-  return <View>olá</View>;
+  return <View></View>;
 }
--- aulas/mobile/src/pages/Incidents/index.js
+++ aulas/mobile/src/pages/Incidents/index.js
@@ -3,5 +3,5 @@ import React from "react";
 import { View } from "./styles";
 
 export default function Incidents() {
-  return <View>olá</View>;
+  return <View></View>;
 }
