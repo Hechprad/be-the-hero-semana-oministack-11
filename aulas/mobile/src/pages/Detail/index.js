8b3fc0dc0b73701a01267ce2fb00e00427f32082
Jorge Hecherat <hecherat@gmail.com>
2020-04-01 15:42:14 -0300

    [mobile] - Details page

--- aulas/mobile/src/pages/Detail/index.js
+++ aulas/mobile/src/pages/Detail/index.js
@@ -1,7 +1,46 @@
 import React from "react";
 
-import { View } from "./styles";
+import logoImg from "../../assets/logo.png";
+
+import Incident from "../../components/Incident";
+
+import {
+  Action,
+  Actions,
+  ActionText,
+  ContactBox,
+  Container,
+  FeatherIcon,
+  HeroDescription,
+  HeroTitle,
+  Image,
+  TouchableOpacity,
+  Header
+} from "./styles";
 
 export default function Details() {
-  return <View></View>;
+  return (
+    <Container>
+      <Header>
+        <Image source={logoImg} />
+        <TouchableOpacity onPress={() => {}}>
+          <FeatherIcon name={"arrow-left"} size={28} />
+        </TouchableOpacity>
+      </Header>
+      <Incident />
+      <ContactBox>
+        <HeroTitle>Salve o dia!</HeroTitle>
+        <HeroTitle>Seja o herói desse caso.</HeroTitle>
+        <HeroDescription>Entre em contato:</HeroDescription>
+        <Actions>
+          <Action></Action>
+          <ActionText>Whatsapp</ActionText>
+        </Actions>
+        <Actions>
+          <Action></Action>
+          <ActionText>E-mail</ActionText>
+        </Actions>
+      </ContactBox>
+    </Container>
+  );
 }
--- aulas/mobile/src/pages/Detail/styles.js
+++ aulas/mobile/src/pages/Detail/styles.js
@@ -1,5 +1,53 @@
 import styled from "styled-components/native";
+import Constants from "expo-constants";
+import { Feather } from "@expo/vector-icons";
 
-export const View = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  padding: ${Constants.statusBarHeight + 20}px 24px;
+`;
 
-export const Text = styled.Text``;
+export const Header = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Image = styled.Image``;
+
+export const TouchableOpacity = styled.TouchableOpacity`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const FeatherIcon = styled(Feather).attrs(props => ({
+  name: props.name,
+  size: props.size
+}))`
+  color: ${props => props.theme.colors.red};
+`;
+
+export const ContactBox = styled.View``;
+
+export const HeroTitle = styled.Text`
+  font-size: 30px;
+  margin-bottom: 16px;
+  margin-top: 48px;
+  color: ${props => props.theme.colors.black};
+  font-weight: bold;
+`;
+
+export const HeroDescription = styled.Text`
+  font-size: 16px;
+  margin-bottom: 16px;
+  margin-top: 48px;
+  color: ${props => props.theme.colors.black};
+  font-weight: bold;
+`;
+
+export const Actions = styled.View``;
+
+export const Action = styled.TouchableOpacity``;
+
+export const ActionText = styled.Text``;
