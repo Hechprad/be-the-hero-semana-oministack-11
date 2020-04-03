119ac8825f5526241d399293062e7e58046e7392
Jorge Hecherat <hecherat@gmail.com>
2020-04-03 15:22:18 -0300

    [backend] - Some validations added

--- aulas/backend/src/routes.js
+++ aulas/backend/src/routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { celebrate, Segments, Joi } = require("celebrate");
 
 const SessionController = require("./controllers/SessionController");
 
@@ -11,12 +12,50 @@ const routes = express.Router();
 routes.post("/sessions", SessionController.create);
 
 routes.get("/ongs", OngController.index);
-routes.post("/ongs", OngController.create);
+
+routes.post(
+  "/ongs",
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string()
+        .required()
+        .email(),
+      whatsapp: Joi.number()
+        .required()
+        .min(10)
+        .max(11),
+      city: Joi.string().required(),
+      uf: Joi.string()
+        .required()
+        .length(2)
+    })
+  }),
+  OngController.create
+);
 
 routes.get("/profile", ProfileController.index);
 
 routes.get("/incidents", IncidentController.index);
-routes.post("/incidents", IncidentController.create);
-routes.delete("/incidents/:id", IncidentController.delete);
+
+routes.post(
+  "/incidents",
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      title: Joi.string().required(),
+      description: Joi.string().required(),
+      value: Joi.number().required()
+    })
+  }),
+  IncidentController.create
+);
+
+routes.delete(
+  "/incidents/:id",
+  // celebrate({
+  //   [Segments.PARAMS]: Joi.value()
+  // }),
+  IncidentController.delete
+);
 
 module.exports = routes;
