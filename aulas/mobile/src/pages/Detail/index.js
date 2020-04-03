fa7dd557bbe9b163672d4204661674b794ae27f8
Jorge Hecherat <hecherat@gmail.com>
2020-04-03 09:09:11 -0300

    [mobile] - Detail page with API integration

--- aulas/mobile/src/pages/Detail/index.js
+++ aulas/mobile/src/pages/Detail/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Linking } from "react-native";
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
 import * as MailComposer from "expo-mail-composer";
 
 import logoImg from "../../assets/logo.png";
@@ -23,18 +23,28 @@ import {
 
 export default function Details() {
   const navigation = useNavigation();
+  const route = useRoute();
+
+  const incident = route.params.incident;
 
   const navigateBack = () => {
     navigation.goBack();
   };
 
-  const cel = "5511999999999";
-  const message = `Olá APAD, estou entrando em contato pois gostaria de ajudar no caso "Cadelinha atropelada" com o valor de R$ 120,00`;
+  const cel = incident.whatsapp;
+  const message = `Olá ${
+    incident.name
+  }, estou entrando em contato pois gostaria de ajudar no caso "${
+    incident.title
+  }" com o valor de ${Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL"
+  }).format(incident.value)}`;
 
   const sendMail = () => {
     MailComposer.composeAsync({
-      subject: "Herói do caso: Cadelinha atropelada",
-      recipients: ["hecherat@gmail.com"],
+      subject: `Herói do caso: ${incident.title}`,
+      recipients: [incident.email],
       body: message
     });
   };
@@ -42,7 +52,7 @@ export default function Details() {
   const sendWhatsapp = () => {
     Linking.openURL(`whatsapp://send?phone=${cel}&text=${message}`);
   };
-
+  console.log(incident);
   return (
     <Container>
       <Header>
@@ -51,7 +61,7 @@ export default function Details() {
           <FeatherIcon name={"arrow-left"} size={28} />
         </TouchableOpacity>
       </Header>
-      <Incident style={{ marginTop: 48 }} />
+      <Incident style={{ marginTop: 48 }} incident={incident} />
       <ContactBox>
         <HeroTitle>Salve o dia!</HeroTitle>
         <HeroTitle>Seja o herói desse caso.</HeroTitle>
